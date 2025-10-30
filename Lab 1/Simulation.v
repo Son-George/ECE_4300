bc3492a6585eee60f404f2c2523925ef31da3dfc
Son-George <156357640+Son-George@users.noreply.github.com>
2025-10-30 15:33:20 -0700

    Add files via upload

---
+++ Lab 1/Simulation.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/30/2025 03:03:25 PM
+// Design Name: 
+// Module Name: Simulation
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Simulation();
+
+    reg PCSrc, clk;
+    reg [31:0] ExMem;
+    wire [31:0] Instr, Address;
+    
+    Test_bench sim (.clk(clk), .PCSrc(PCSrc), .ex_mem(EXMem), .if_id_instr(Instr), .if_id_addrs(Address));
+    
+    // creates simulation clock
+    initial forever #5 clk = ~clk;
+    
+    initial begin
+    PCSrc = 0;
+    ExMem = 32'h0000FFFF;
+    #8
+    PCSrc = 1'b1;
+    #5
+    PCSrc = 1'b0;
+    #30
+    ExMem = 32'h0000FFFF;
+    end
+endmodule
---
+++ Lab 1/TestBench.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/30/2025 02:06:43 PM
+// Design Name: 
+// Module Name: TestBench
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module TestBench(
+    input clk, PCSrc,
+    input [31:0] ex_mem,
+    output wire [31:0] if_id_instr, if_id_addrs
+    );
+    wire [31:0] mux_out, pc_out, instr_out, adder_out, instr_out;
+    
+    Mux m1 (.Increment(adder_out), .PC(ex_mem), .New_Address(mux_out), .PC_Select(PCSrc));
+    
+    PC pc1 (.PC_in(mux_out), .CLK(clk), .PC_out(pc_out));
+    
+    Adder add1 (.Instr(pc_out), .New_Instr(adder_out));
+    
+    Instr_Mem instr1 (.Address(pc_out), .clk(clk), .Instr(instr_out));
+    
+    Latch L1 (.Address(adder_out), .Instr(instr_out), .CLK(clk), .New_Address(if_id_instr), New_Instr(if_id_addrs));
+    
+endmodule
