12c6db39c265b10bf3b0d929c691a396478238a2
Son-George <156357640+Son-George@users.noreply.github.com>
2025-11-05 11:23:38 -0800

    Update TestBench.v

--- Lab 1/TestBench.v
+++ Lab 1/TestBench.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 10/30/2025 02:06:43 PM
+// Create Date: 10/16/2025 04:21:25 PM
 // Design Name: 
-// Module Name: TestBench
+// Module Name: Latch
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,21 +20,27 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module TestBench(
-    input wire clk, PCSrc,
-    input wire [31:0] ex_mem,
-    output wire [31:0] id_instr, id_addrs
+
+
+module Latch(
+    input [31:0] pc_in, //program counter +4
+    input [31:0] Instr, //instruction from memory
+    input wire clk,
+    input wire rst,
+    output reg [31:0] pc_out, //new addy program counter +4 to ID stage
+    output reg [31:0] instr_out //data, instrcution to ID stage
     );
-    wire [31:0] mux_out, pc_out, instr_out, adder_out, mem_out;
     
-    Mux m1 (.Increment(adder_out), .PC(ex_mem), .New_Address(mux_out), .PC_Select(PCSrc));
+    always @(posedge clk or posedge rst)
+    begin
+    if (rst) begin
+        pc_out <= 32'd0;
+        instr_out<=32'd0;
+    end else begin
+        pc_out <= pc_in; //store PC+4
+        instr_out <= Instr; //store inst.
+    end
     
-    PC pc1 (.PC_in(mux_out), .CLK(clk), .PC_out(pc_out));
-    
-    Adder add1 (.Instr(pc_out), .New_Instr(adder_out));
-    
-    Instr_Mem instr1 (.Address(pc_out), .CLK(clk), .Instr(mem_out));
-    
-    Latch L1 (.Address(adder_out), .Instr(instr_out), .CLK(clk), .New_Address(id_instr), .New_Instr(id_addrs));
+end
     
 endmodule
