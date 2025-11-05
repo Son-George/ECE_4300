61646d04caae60e4253ab8f1dafc6c581ee4424f
Son-George <156357640+Son-George@users.noreply.github.com>
2025-11-05 11:19:41 -0800

    Update Mux.v

--- Lab 1/Mux.v
+++ Lab 1/Mux.v
@@ -21,16 +21,18 @@
 
 
 module Mux(
-    input [31:0] Increment,
-    input [31:0] PC,
-    output reg [31:0] New_Address,
+    input [31:0] a_true,
+    input [31:0] b_false,
+    output reg [31:0] y, //New_Address
     input PC_Select
     );
     always @(*) 
-    bgein
+    begin
         case (PC_Select)
-            1'b0: New_Address <= Increment;
-            1'b1: New_Address <= PC;
+            //when program counter is 1, itll branch
+            //when program couter is 0, ill increment + 4 to next inst and normal flow
+            1'b0: y = b_false; //normal flow = program counter + 4
+            1'b1: y = a_true; //branch jump 
         endcase;
     end
 endmodule
